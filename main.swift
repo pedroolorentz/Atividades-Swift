b6c659c6d43d37ea0c2650dfebd17df0e745aad6
Pedro <69764768+pedroolorentz@users.noreply.github.com>
2021-03-01 18:16:23 -0300

    1a Atividade Swift

---
+++ main.swift
@@ -0,0 +1,33 @@
+// numero 1
+var num: Int
+num = 5
+
+var total: Int = 6
+
+var resultado = 7
+
+// numero 2
+var carName: String = "carro"
+var carCode: Int = 3215642
+var preco: Double = 51.999
+var clienteNome: String = "Jucimara"
+var enderecoCliente = "rua x"
+
+// numero 3
+
+//Digita anotações em Swift e outros tipos de variáveis ​​recebidas, como strings, inteiros, etc.
+
+// numero 4
+
+var int: Int = 2
+var double: Double = 2.2
+var text = String(double) + " " + String(int)
+print (text)
+
+// numero 5
+
+//safety:
+//É uma linguagem que o incentiva a especificar os tipos de variáveis.
+
+//inference:
+//O tipo da variável não é especificado, e a linguagem irá deduzir o tipo da variável com base no valor seguido.
